d1c94e7ebcf57f7e19974b85e3a49a7e9f805a3a
riyaG30 <riya.rg3010@gmail.com>
2024-01-05 14:35:39 +0530

    new file:   client/public/images/favicon.ico 	new file:   client/public/images/load.gif

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,41 @@
-import { BrowserRouter,Routes, Route} from "react-router-dom";
-import Home from "./components/Home";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import './App.css'; // Import your CSS file
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const spinner = document.getElementById('spinner');
+
+    if (spinner) {
+      setTimeout(() => {
+        spinner.style.opacity = '0'; // Add opacity animation
+        setTimeout(() => {
+          spinner.style.display = 'none';
+          setLoading(false);
+        }, 500); // Adjust the timeout as needed
+      }, 3300);
+    }
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" Component={Home}/>
-      </Routes>
-    </BrowserRouter>
-  )
+    <div>
+      {loading ? (
+        <div className="container" id="spinner">
+          <div className="loading"></div>
+          <img src="/images/load.gif" alt="Loading..." />
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
