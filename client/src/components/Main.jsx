64261013edeb2a8ac284659809a1f7036f884fc7
riyaG30 <riya.rg3010@gmail.com>
2024-01-08 20:43:13 +0530

    modified:   client/src/components/Main.jsx

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,3 +1,8 @@
+// Main.js
+
+import React from 'react';
+import './Main.css';
+
 const Main = () => {
     return (
         <main className='h-[80%] flex justify-between items-center'>
@@ -5,27 +10,35 @@ const Main = () => {
                 <div className='h-[60%] w-[55%]'>
                     <h1 className='text-[90px] ml-[-5px]'>ProfNITT</h1>
                     <p className='text-3xl mt-[-20px]'>The Finance and Investments club of NIT Trichy</p>
-                    <div className='flex h-[15%] w-[80%] mt-5 justify-between'>
-                        <button className='cursor-pointer w-[40%] border-none rounded bg-[#0328EE] hover:bg-white hover:text-black'>Download App</button>
-                        <button className='cursor-pointer w-[40%] border-none rounded bg-[#0328EE] hover:bg-white hover:text-black'>View Pricing</button>
-                    </div>
+                    
+                    <p className='text-3xl mt-[-20px]'>
+                        <span className='animation-container'>
+                            <span className="content__container__text">We are </span>
+                            <ul className="content__container__list">
+                                <li className="content__container__list__item">Profit !</li>
+                                <li className="content__container__list__item">Research !</li>
+                                <li className="content__container__list__item">Quant !</li>
+                                <li className="content__container__list__item">Finance !</li>
+                            </ul>
+                        </span>
+                    </p>
                 </div>
             </div>
             <div className=' h-full'>
                 <div className="bg-[url('/images/laptop_body.svg')] h-[90%] ml-20 flex bg-no-repeat justify-end bg-right">
                     <div>
-                        <img src="/images/laptop_screen.svg" alt="laptop screen" className="w-full h-full"/>
+                        <img src="/images/laptop_screen.svg" alt="laptop screen" className="w-full h-full" />
                     </div>
                 </div>
                 <div>
                     <div>
-                    <img src="/images/BaseBody.svg" alt="Base body" className='' />
-                    <img src="/images/FootBase.svg" alt="laptop" className='ml-16' />
+                        <img src="/images/BaseBody.svg" alt="Base body" className='' />
+                        <img src="/images/FootBase.svg" alt="laptop" className='ml-16' />
                     </div>
                 </div>
             </div>
         </main>
-    )
-}
+    );
+};
 
-export default Main
+export default Main;
