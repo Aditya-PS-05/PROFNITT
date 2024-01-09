ee3bb8af3f97c4b43f0a9cf48f89ce2836e150c2
Hyphenhypen <adityapratapsjnhh7654@gmail.com>
2024-01-09 10:24:40 +0530

    laptop image adjustments

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,6 +1,6 @@
 const Main = () => {
     return (
-        <main className='h-[80%] flex justify-between items-center'>
+        <main className='h-[80%] md:flex justify-between items-center'>
             <div className='text-white h-full w-[50%] flex justify-center items-center'>
                 <div className='h-[60%] w-[55%]'>
                     <h1 className='text-[90px] ml-[-5px]'>ProfNITT</h1>
@@ -11,15 +11,15 @@ const Main = () => {
                     </div>
                 </div>
             </div>
-            <div className=' h-full'>
-                <div className="bg-[url('/images/laptop_body.svg')] h-[90%] ml-20 flex bg-no-repeat justify-end bg-right">
+            <div className='h-full border-2'>
+                <div className="bg-[url('/images/laptop_body.svg')] h-[90%] ml-[130px] flex bg-no-repeat justify-end bg-right border-none rounded-t-[10px] bg-cover">
                     <div>
-                        <img src="/images/laptop_screen.svg" alt="laptop screen" className="w-full h-full"/>
+                        <img src="/images/laptop_screen.svg" alt="laptop screen" className="w-full h-full" id="laptopScreen"/>
                     </div>
                 </div>
                 <div>
                     <div>
-                    <img src="/images/BaseBody.svg" alt="Base body" className='' />
+                    <img src="/images/BaseBody.svg" alt="Base body" className='ml-10' />
                     <img src="/images/FootBase.svg" alt="laptop" className='ml-16' />
                     </div>
                 </div>
