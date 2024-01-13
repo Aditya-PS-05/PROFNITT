bb6c9a93c984c6092ebea537998b5ef7154ff2f6
riyaG30 <riya.rg3010@gmail.com>
2024-01-13 11:48:08 +0530

    modified:   client/index.html 	modified:   client/src/components/About.css 	modified:   client/src/components/About.jsx

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,12 +4,19 @@ const About = () => {
   return (
     <div className='flex flex-col overflow-'>
       <div className='flex items-center justify-center'>
-        <div className='p-6 m-10 text-1xl border-2 border-slate-500 text-center w-[700px] rounded-[10px]'>
-          <h1 className='text-7xl text-center mb-[20px]'>Abouts</h1>
-           <p>Being one of the pioneer fintech clubs of the institute, established in 2018, the melange of projects and domains that we, as Profnitt, delve into has always been ever-expanding. With the vision to  inculcate healthy and fruitful discussions on markets and the economical state of the world and provides an opportunity to students to get practical experience in the markets.
-            We also take part in competitions, fintech hackathons and symposiums in addition to conducting workshops and events to further the spirit of learning and growth among all.
-          </p> 
-        </div>
+      <div className='p-6 m-10 text-xl border-2 border-slate-500 text-center w-[800px] rounded-[10px]'>
+    <h1 id="abouts">Abouts</h1>
+    <div id="about-data">
+        <p>ProfNITT is the official Finance and Investment club of NIT Trichy. We are a close-knit community of finance enthusiasts from diverse backgrounds who take an interest in finance, stock market, options, and quants to spread the elegance of finance among the student community of NIT Trichy.</p>
+        <br />
+        <br />
+        <p>Being one of the pioneer fintech clubs of the institute, established in 2018, the melange of projects and domains that we, as Profnitt, delve into has always been ever-expanding. With the vision to inculcate healthy and fruitful discussions on markets and the economical state of the world and provides an opportunity to students to get practical experience in the markets.</p>
+        <br />
+        <br />
+        <p>We also take part in competitions, fintech hackathons and symposiums in addition to conducting workshops and events to further the spirit of learning and growth among all.</p>
+    </div>
+</div>
+
       </div>
       <div className='w-full h-full flex-grow flex justify-center items-center mt-10'>
         <div className='w-[30%] h-full flex justify-center items-center relative'>
