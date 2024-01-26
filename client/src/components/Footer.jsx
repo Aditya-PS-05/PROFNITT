3c6d2e9a917ed198e84d815393398535e2ce1f44
riyaG30 <riya.rg3010@gmail.com>
2024-01-26 16:24:57 +0530

    modified:   client/src/components/Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,35 +3,34 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='w-screen flex justify-center items-center mt-10'>
-      <div className='w-[98%] bg-[#010D50] md:px-16 md:py-7'>
-        <div className='md:flex justify-evenly items-center w-full'>
-          <div className='w-[40%]'>
-            <div className='flex gap-[10px] items-center'>
-              <img src="/images/logo.png" alt="ProfNITT Logo" className='w-[30%]' />
-              <h1 className='text-[2vmax] font-DMSans font-bold'>Finance and Investment Club</h1>
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-              <h1 className='font-ppSupplyMono'>Follow us on</h1>
-              <div className='flex gap-3 mt-3'>
-                <div className=''><img src="/images/instagram.svg" alt="Instagram" /></div>
-                <div><img src="/images/facebook.svg" alt="Instagram" /></div>
-                <div><img src="/images/linkedin.svg" alt="Instagram" /></div>
+      <div className='w-[100%] bg-[#010D50] md:px-16 md:py-7'>
+        <div className='md:flex justify-center items-center w-full'>
+          <div className='w-[40%] flex justify-center items-center flex-col'>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <img src="/images/logo.png" alt="ProfNITT Logo" className='h-[90px]' style={{ marginTop: '-20px', marginRight: '15px' }} />
+              <div>
+                <br />
+                <h1 className='text-[1vmax] font-DMSans font-bold' style={{ padding: 0, fontSize: '20px', fontFamily: 'Poppins, sans-serif' }}>The Finance and</h1>
+                <h1 className='text-[1vmax] font-DMSans font-bold' style={{ padding: 0, fontSize: '20px', fontFamily: 'Poppins, sans-serif' }}> Investment Club</h1>
               </div>
             </div>
+            <div className='font-ppSupplyMono' style={{ color: '#57E2E5',padding: 0, fontSize: '20px', fontFamily: 'Poppins, sans-serif', marginTop: '10px', marginLeft: '-250px' }}>Follow Us</div> {/* Adjusted margin-left */}
+            <div className='flex gap-3 mt-3' style={{ marginLeft: '-200px' }}> {/* Adjusted margin-left */}
+              <div><a href="https://www.instagram.com/profnitt?igsh=bTd0ZW1oMGQyeHA3" target="_blank"><img src="/images/instagram.svg" alt="Instagram" /></a></div>
+              <div><a href="https://www.facebook.com/profnitt?mibextid=ZbWKwL" target="_blank"><img src="/images/facebook.svg" alt="Facebook" /></a></div>
+              <div><a href="https://www.linkedin.com/company/profnitt/" target="_blank"><img src="/images/linkedin.svg" alt="LinkedIn" /></a></div>
+            </div>
           </div>
-          <div className='rounded-[10px] bg-[#0328EE] p-4'>
-            <h1 className='font-PPSupplyMono font-bold'>Contact Us</h1>
-            <h1 className='font-light'>NIT Trichy</h1>
-            <h1 className='font-light'>+91 870994638</h1>
-            <h1 className='font-light'>profnitt.club@gmail.com</h1>
-          </div>
+          <div className='w-[60%]'></div>
         </div>
+        <br style={{ borderTop: '2px solid #000', margin: '20px 0', display: 'block' }} />
+        <hr />
         <div className='text-center mt-6'>
-          <h1 className='font-DMSans'>Designed and Deployed by profNITT</h1>
+          <h1 className='font-DMSans' style={{ color: '#57E2E5', padding: 0, fontSize: '20px', fontFamily: 'Poppins, sans-serif' }}>Designed and Deployed by ProfNITT Webteam</h1>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
