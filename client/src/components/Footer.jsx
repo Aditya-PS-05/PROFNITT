bb9ab4b6a032a6e2dea37726b71d248feab582e6
riyaG30 <riya.rg3010@gmail.com>
2024-01-26 23:28:16 +0530

    modified:   client/src/components/Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -21,7 +21,25 @@ const Footer = () => {
               <div><a href="https://www.linkedin.com/company/profnitt/" target="_blank"><img src="/images/linkedin.svg" alt="LinkedIn" /></a></div>
             </div>
           </div>
-          <div className='w-[60%]'></div>
+          <div className='w-[50%]'>
+            <div style={{ marginLeft: '280px' }}> {/* Adjusted margin-left */}
+              <h1 style={{ padding: 0, fontSize: '30px', fontFamily: 'Poppins, sans-serif' }}> <b>Contact Information</b></h1>
+              
+              <p style={{ display: 'flex', alignItems: 'center' }}>
+      <img src="/images/location.svg" alt="Location Icon" style={{ marginRight: '5px' }} />
+      <span> Scient Room, NIT Trichy </span>
+    </p>
+    <p style={{ display: 'flex', alignItems: 'center' }}>
+      <img src="/images/phone.svg" alt="Location Icon" style={{ marginRight: '5px' }} />
+      <span> +91 8709921638 </span>
+    </p>
+    <p style={{ display: 'flex', alignItems: 'center' }}>
+      <img src="/images/phone.svg" alt="Location Icon" style={{ marginRight: '5px' }} />
+      <span> riya.rg3010@gmail.com </span>
+    </p>
+            
+            </div>
+          </div>
         </div>
         <br style={{ borderTop: '2px solid #000', margin: '20px 0', display: 'block' }} />
         <hr />
