1ebc1e99fc25a560409d9fd3d43733fea2d01337
riyaG30 <riya.rg3010@gmail.com>
2024-02-03 16:17:32 +0530

    modified:   client/src/components/Home page/Navbar.jsx

--- client/src/components/Home page/Main.jsx
+++ client/src/components/Home page/Main.jsx
@@ -19,7 +19,7 @@ const Main = () => {
     }, []);
 
     return (
-        <main className='mt-10 md:flex justify-between items-center border-2 border-red-500 relative z-[1000] mb-12' style={{ paddingTop: '95px' }}>
+        <main className=' md:flex justify-between items-center border-2 border-red-500 relative z-[1000] mb-12' style={{ paddingTop: '95px' }}>
        
       
             {/* <div className='h-[550px] w-[550px] rounded-[50%] bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-indigo-900 to-[#010725] absolute bottom-[-20%] right-[20%] z-0 shadow-2xl shadow-[#010725] blur-[150px] border-2 border-red-500' /> */}
