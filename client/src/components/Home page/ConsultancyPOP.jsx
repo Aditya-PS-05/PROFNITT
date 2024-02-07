10605af82f7dcf51e0ae1a5431bc32b450d9876e
riyaG30 <riya.rg3010@gmail.com>
2024-02-07 12:27:56 +0530

    modified:   client/src/components/Home page/ConsultancyPOP.jsx

--- client/src/components/Home page/ConsultancyPOP.jsx
+++ client/src/components/Home page/ConsultancyPOP.jsx
@@ -35,11 +35,9 @@ function ConsultancyPOP() {
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Centered Modal</h4>
+          <h4>Heading</h4>
           <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
+            Riya loves Ambareesh
           </p>
         </Modal.Body>
         <Modal.Footer>
