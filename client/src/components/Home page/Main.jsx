0c086e21d5c4c6783f990b4025bc4756c926516a
Hyphenhypen <adityapratapsjnhh7654@gmail.com>
2024-02-20 19:03:13 +0530

    main page

--- client/src/components/Home page/Main.jsx
+++ client/src/components/Home page/Main.jsx
@@ -5,11 +5,11 @@ import './Main.css';
 const Main = () => {
 
     return (
-        <main className='md:h-screen md:flex justify-between items-center relative z-[1000] mb-12 pt-[85px]'>
+        <main className='md:flex justify-between items-center relative z-[1000]'>
             <div className='h-[550px] w-[550px] rounded-[50%] bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-indigo-900 to-[#010725] absolute bottom-[-20%] right-[20%] z-0 shadow-2xl shadow-[#010725] blur-[150px] border-2 border-red-500' />
-            <div className='md:flex justify-between items-center  w-full h-[90%]'>
+            <div className='md:flex justify-between items-center w-full h-[90%]'>
             <div className='text-white h-full ml-[6vw] md:w-[40%] sm:w-full flex justify-start items-center z-[2000000] '>
-                <div className='w-[75%] sm:mt-10 mt-6 md:mt-0 md:ml-10 z-[100000]'>
+                <div className='w-[75%] sm:mt-10 mt-6 md:mt-0 md:ml-0 z-[100000]'>
                     <img src="/images/ProfNITT_text.svg" alt="ProfNITT text image" />
                     <p className='text-2xl font-thin font-PPSupplyMono mt-4'>The Finance and Investments club of NIT Trichy</p>
                     <div className='text-3xl font-PPSupplyMono font-thin'>
@@ -25,9 +25,9 @@ const Main = () => {
                     </div>
                 </div>
             </div>
-            <div className='w-full h-full sm:mt-10 md:w-[45%] relative z-[2000000]'>
-                <div className="bg-[url('/images/laptop_body.svg')] h-[87%] ml-[130px] flex bg-no-repeat justify-end bg-right border-none rounded-t-[10px] bg-cover relative z-[1000] mt-4">
-                    <div className='h-full w-full'>
+            <div className='w-full h-full md:w-[45%] relative z-[2000000]'>
+                <div className="bg-[url('/images/laptop_body.svg')] h-[83%] ml-[130px] flex bg-no-repeat justify-end bg-right border-none rounded-t-[10px] bg-cover relative z-[1000] mt-8">
+                    <div className='h-full w-full '>
                         <img src="/images/laptop.svg" alt="laptop screen" className="w-full" id='laptop-screen' />
                     </div>
                 </div>
